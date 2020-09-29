fc2d28856de4fef6993ebbe7f83c762ed008e69e
kartikdutt18 <kartikdutt@live.in>
2020-09-29 22:39:39 +0530

    Modified visualize.py to support multiple files

--- src/visualize_input.py
+++ src/visualize_input.py
@@ -2,6 +2,13 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 
+query = input('Enter Type of Encoding')
+path = './data/data_Binary_NRZ_RX(small).csv'
+if (query == 'NRZ') :
+	path = "./data/data_Binary_NRZ_RX(small).csv"
+else :
+	path = "./data/data_PAM4_RX(small).csv"
+
 data = np.genfromtxt('src/data/data_Binary_NRZ_RX(small).csv', delimiter=',', names=['Time', 'ElectricalSignal'])
 plt.plot(data['Time'], data['ElectricalSignal'])
 plt.xlabel('Time')
